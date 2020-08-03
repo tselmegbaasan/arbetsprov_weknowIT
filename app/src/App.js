5211a1157f5c84c2e9189af02bce2fdcb5d4926a
tselmegbaasan <ts2530ba-s@student.lu.se>
2020-08-03 19:22:12 +0200

    Code added for new react components such as searchbycity and searchbycountry

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
+import ResultPopulation from "./components/ResultPopulation";
+import ResultCountry from "./components/ResultCountry";
 import SearchByCity from "./components/SearchByCity";
 import SearchByCountry from "./components/SearchByCountry";
 import Menu from "./components/Menu";
@@ -18,6 +20,8 @@ class App extends Component {
           <div className="content">
             <Switch>
               <Route exact path="/" component={Menu} />
+              <Route path="/searchbycity/result/:cityName" component={ResultPopulation} />
+              <Route path="/searchbycountry/result/:countryName" component={ResultCountry} />
               <Route exact path="/searchbycity" component={SearchByCity} />
               <Route exact path="/searchbycountry" component={SearchByCountry} />
             </Switch>
