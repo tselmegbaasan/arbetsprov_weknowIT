3a6058d87bf8d606982e90fdb6f17d79b9b8d132
tselmegbaasan <ts2530ba-s@student.lu.se>
2020-08-06 15:36:07 +0200

    Calls to API are more correct now

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const username = "&username=weknowit";
 
 exports.getByCity = (req, res) => {
     const city_name = req.params.city;
-    fetch(base_url + `name_equals=${city_name}` + "&featureCode=PPLA2&featureCode=PPLA&featureCode=PPLC" + username, {
+    fetch(base_url + `q=${city_name}` + "&featureCode=PPLA&featureCode=PPLC&featureCode=PPLA2&featureCode=PPLA3&orderby=population" + username, {
         method: "GET"
     })
         .then(response => response.json())
@@ -19,6 +19,33 @@ exports.getByCity = (req, res) => {
         .catch(err => {
             res.status(500).send({
                 message: err.message || "Some error occurred while retrieving data about: " + city_name
+            })
+        })
+}
+
+exports.getByCountry = (req, res) => {
+    const country_name = req.params.country;
+    fetch(base_url + `q=${country_name}` + "&maxRows=10" + '&featureCode=PPLA&featureCode=PPLC' + `&orderby=population` + username, {
+        method: "GET"
+    })
+        .then(response => response.json())
+        .then(data => {
+            let result = [];
+            data.geonames.forEach(object => {
+                if (encodeURI(object.countryName.toLowerCase()) === encodeURI(country_name.toLowerCase())) {
+                    result.push(object);
+                }
+            })
+            res.send({
+                first_city: result[0].name,
+                second_city: result[1].name,
+                third_city: result[2].name
+            }
+            );
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving data for " + country_name
             })
         })
 }
@@ -48,25 +75,3 @@ formatNumber = (number) => {
 
     return list.join("").toString();
 }
-
-exports.getByCountry = (req, res) => {
-    const country_name = req.params.country;
-    fetch(base_url + `q=${country_name}` + "&maxRows=3" + '&featureCode=PPLA&featureCode=PPLC' + `&orderby=population` + username, {
-        method: "GET"
-    })
-        .then(response => response.json())
-        .then(data => {
-            res.send({
-                first_city: data.geonames[0].name,
-                second_city: data.geonames[1].name,
-                third_city: data.geonames[2].name
-            }
-            );
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while retrieving data for " + country_name
-            })
-        })
-}
-
